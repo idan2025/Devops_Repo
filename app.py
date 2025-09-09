4e6f46b7787a150263f0a6ef26b90c55f2120cc4
idan2025 <idan3330@yahoo.com>
2025-09-09 20:51:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return "Hello World, Built with Jenkinsfile"
 
 if __name__ == "__main__":
     # Bind to all interfaces so Docker can map the port
